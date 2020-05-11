20ca646ebf5f531a517562327c939bbe7f37c5dc
Derrick A Wood <dwood_1994@yahoo.com>
2020-05-11 14:25:04 -0600

    number game

--- Program.cs
+++ Program.cs
@@ -7,6 +7,48 @@ namespace numbergame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+
+
+            System.Console.WriteLine("Would you like to guess a number y/n?");
+            string userInput = Console.ReadLine();
+            bool playing = userInput.ToLower() == "y";
+            Random random = new Random();
+            int numberToGuess = random.Next(1,10);
+            while(playing)
+            {
+                System.Console.WriteLine("pick a number between 1 and 10");
+                string guess = Console.ReadLine();
+                int guessedNumber;
+                if(!int.TryParse(guess, out guessedNumber) || guessedNumber > 10 || guessedNumber < 1)
+                {
+                    System.Console.WriteLine("not a valid number");
+                    continue;
+                }
+                if(guessedNumber == numberToGuess)
+                {
+                    System.Console.WriteLine("you win! would you like to guess again? y/n");
+                    string keepPlaying = Console.ReadLine();
+                    if(keepPlaying.ToLower() == "y")
+                    {
+                        numberToGuess = random.Next(1,10);
+                    } 
+                    else 
+                    {
+                        playing = false;
+                        continue;
+                    }
+                }
+                else if(numberToGuess > guessedNumber)
+                {
+                    System.Console.WriteLine("too low");
+                }
+                else if(numberToGuess < guessedNumber)
+                {
+                    System.Console.WriteLine("too high");
+                }
+            }
+            }
+
+
     }
 }
