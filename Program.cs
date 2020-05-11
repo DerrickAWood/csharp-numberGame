294cc6d37a7e1fe81ec7b28f50c559e7f571c5b7
Derrick A Wood <dwood_1994@yahoo.com>
2020-05-11 13:30:15 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace numbergame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
